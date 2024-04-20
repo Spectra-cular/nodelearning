cb8c060e653cfe96eb7c149bd32755f776248460
Spectra-cularecho <pawanpanti9@gmail.com>
2024-04-20 23:16:01 +0545

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+// const app = require("express")()
+const express = require('express')
+const app = express()
 
-const express=require('express');
-const app=express();
-
-//app.use(express.static(__dirname+'/dist/'));
-
+require("./model/index")
+app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
-    res.send("Hey Hi")
+    const data = {name : "Pawan",age:19}
+    res.render('home',{data})
 })
 
+app.get("/blog",(req,res)=>{
+    res.render('blog')
+})
+
+app.get("/blog/create",(req,res)=>{
+    res.render("createblog")
+})
+
+app.get("/blog/edit",(req,res)=>{
+    res.render('editblog')
+})
+
+
 app.listen(3000,()=>{
-    console.log("Server hast started at portnumber 3000")
+    console.log("Server has started at port 3000")
 })
